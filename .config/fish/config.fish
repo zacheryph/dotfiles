74442009cd4ed08dd6ac722900ea6a81a33616f9
Zachery Hostens <zacheryph@gmail.com>
2016-06-28 23:10:19 -0500

    add fish shell configuration

---
+++ .config/fish/config.fish
@@ -0,0 +1,22 @@
+# Defaults {{{
+set -gx EDITOR vim
+set -gx LSCOLORS Gxfxcxdxbxegedabagacad
+# }}}
+# Paths {{{
+set -gx PATH $GOPATH/bin /usr/local/opt/go/libexec/bin /usr/local/sbin /usr/local/bin $PATH
+
+set -gx ANDROID_HOME /usr/local/opt/android-sdk
+set -gx GOPATH $HOME/.go
+# }}}
+# Ruby {{{
+set -gx RBENV_ROOT /usr/local/var/rbenv
+
+status --is-interactive; and . (rbenv init -|psub)
+# }}}
+# Aliases {{{
+alias fig "docker-compose"
+alias ranpass "ruby -r securerandom -e 'puts SecureRandom.urlsafe_base64'"
+alias dotfile "git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
+# }}}
+
+# vim:foldmethod=marker:foldlevel=0
