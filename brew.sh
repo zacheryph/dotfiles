3d6267b661a5df07c0f54d82d288e34a86efa351
Zachery Hostens <zacheryph@gmail.com>
2016-08-13 09:51:17 -0500

    add brew script for initial brew installs

---
+++ brew.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+# Taps
+brew tap caskroom/cask
+brew tap grpc/grpc
+brew tap neovim/neovim
+
+# Casks
+brew cask install \
+	atom \
+	rowanj-gitx \
+	google-chrome \
+	keybase \
+	mocksmtp \
+	monolingual \
+	transmission \
+	transmit \
+	vmware-fusion \
+	iterm2
+
+# General
+brew install \
+  cayley \
+  cloc \
+  stormssh \
+  htop \
+  ifstat \
+  irssi \
+  keybase \
+  mtr \
+  nmap \
+  tmux \
+  wrk \
+  youtube-dl
+
+# Development
+brew install \
+  elixir \
+	git \
+	git-extras \
+	git-flow \
+  glide \
+  go \
+  grpc \
+  google-protobuf \
+  neovim \
+	python3 \
+	rbenv \
+	ruby-build \
+	the_silver_searcher
+
+brew install vim \
+  --with-lua \
+  --with-override-system-vi
+
+pip3 install neovim
+
+# Shell
+brew install fish
+echo "/usr/local/bin/fish" | sudo tee -a /etc/shells >/dev/null
+chsh -s /usr/local/bin/fish
