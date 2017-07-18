b638ce4187315f44f15b75f7a24db1ae0b17f3a4
Zachery Hostens <zacheryph@gmail.com>
2017-07-18 15:33:58 -0500

    sudo apt-key not curl

--- Linux/setup.sh
+++ Linux/setup.sh
@@ -115,7 +115,7 @@ run_console() {
 }
 
 run_docker() {
-  sudo curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian
     $(lsb_release -cs)
