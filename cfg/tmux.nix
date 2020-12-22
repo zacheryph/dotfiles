77b2c86af9dd6b5a31862d563e80f8d301a3d98f
Zachery Hostens <zacheryph@gmail.com>
2020-12-22 08:54:45 -0600

    update tmux config management
    Signed-off-by: Zachery Hostens <zacheryph@gmail.com>

--- cfg/tmux.nix
+++ cfg/tmux.nix
@@ -3,27 +3,28 @@
 {
   programs.tmux = {
     enable = true;
+
+    baseIndex = 1;
     clock24 = true;
     escapeTime = 0;
     historyLimit = 10000;
     shortcut = "a";
     terminal = "screen-256color";
 
+    plugins = with pkgs; [
+      tmuxPlugins.sensible
+      tmuxPlugins.pain-control
+      tmuxPlugins.nord
+      {
+        plugin = tmuxPlugins.resurrect;
+        extraConfig = ''
+          set -g @resurrect-strategy-vim 'session'
+          set -g @resurrect-strategy-nvim 'session'
+        '';
+      }
+    ];
+
     extraConfig = ''
-      if "test ! -d ~/.tmux/plugins/tpm" \
-        "run 'git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm'"
-
-      set -g @plugin 'tmux-plugins/tmux-sensible'
-      set -g @plugin 'tmux-plugins/tmux-pain-control'
-      set -g @plugin 'tmux-plugins/tmux-resurrect'
-      set -g @plugin 'arcticicestudio/nord-tmux'
-
-      set -g @resurrect-strategy-vim 'session'
-      set -g @resurrect-strategy-nvim 'session'
-
-      set -g base-index 1
-      setw -g pane-base-index 1
-
       # 24 bit true-color
       set -ga terminal-overrides ",xterm-256color:Tc,screen-256color:Tc"
 
@@ -35,8 +36,6 @@
       set -ga terminal-overrides ',*:Ss=\E[%p1%d q:Se=\E[2 q'
 
       bind -n C-k send-keys "C-l"\; run "tmux clear-history"
-
-      run '~/.tmux/plugins/tpm/tpm'
 
       # TODO: need to be able to set name from machine.
       new-session -d -s 16
