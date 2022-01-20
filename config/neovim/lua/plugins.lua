37fc9bab1ebdecfd740586edbfe13c7949da9f0a
Zachery Hostens <zacheryph@gmail.com>
2022-01-19 23:06:42 -0600

    add fzf extension to allow space in search

--- config/neovim/lua/plugins.lua
+++ config/neovim/lua/plugins.lua
@@ -35,7 +35,12 @@ return require('packer').startup(function(use)
 
   use {
     'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} }
+    requires = 'nvim-lua/plenary.nvim'
+  }
+
+  use {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    run = 'make'
   }
 
   use {
