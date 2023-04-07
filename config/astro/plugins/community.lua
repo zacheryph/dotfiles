a00adde827027c03298366954129cc7d0585c543
Zachery Hostens <zacheryph@gmail.com>
2023-04-06 22:38:45 -0500

    add community packages to astro config

---
+++ config/astro/plugins/community.lua
@@ -0,0 +1,16 @@
+return {
+  -- Add the community repository of plugin specifications
+  "AstroNvim/astrocommunity",
+
+  -- colorschemes
+  { import = "astrocommunity.colorscheme.tokyonight" },
+
+  -- packs
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.nix" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.yaml" },
+}
