0dbfac8215df8b2e018cdb46f7a383de5d14a7c6
Zachery Hostens <zachery@textus.com>
2024-03-18 10:15:50 -0500

    auto suggest enable was renamed

--- config/nix/terminal.nix
+++ config/nix/terminal.nix
@@ -154,7 +154,9 @@
   programs.zsh = {
     enable = true;
     autocd = true;
-    enableAutosuggestions = true;
+    autosuggestion = {
+      enable = true;
+    };
     syntaxHighlighting = {
       enable = true;
     };
