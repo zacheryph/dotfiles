7b9d86dfee1370ad344cf45797976686b1c4260c
Zachery Hostens <zacheryph@gmail.com>
2025-10-23 09:16:18 -0500

    add nixvim flake source and lock

--- flake.nix
+++ flake.nix
@@ -5,18 +5,23 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
+    nixvim = {
+      url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ self, nix-darwin, home-manager, nixpkgs }:
+  outputs = inputs@{ self, home-manager, nix-darwin, nixvim, nixpkgs }:
   let
     system = "aarch64-darwin";
     username = "context";
